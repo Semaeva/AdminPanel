cac3e6f98be0969fe5520fae69b7fa4b4689318f
Semaeva <wunata95@gmail.com>
2022-12-03 15:48:32 +0600

    изменила интерфйес IContent
    создала новые моеди и изменила интерфейс

---
+++ AdminPanel/Interfaces/IPictures.cs
@@ -0,0 +1,11 @@
+﻿using AdminPanel.Models;
+
+namespace AdminPanel.Interfaces
+{
+    public interface IPictures
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
