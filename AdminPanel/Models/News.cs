5c4ce818fb01f72b908b1f66cd31e7ae0e8716f3
bermet <116433177+tartapie@users.noreply.github.com>
2022-12-03 15:27:12 +0600

    ??
    test

--- AdminPanel/Models/News.cs
+++ AdminPanel/Models/News.cs
@@ -5,12 +5,13 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public string? MainPicturePath { get; set; }
 
-        public ICollection<Pictures> Pictures { get; set; }//обратная связь однин-ко-многим
+        public ICollection<NewsPictures> NewsPictures { get; set; }//обратная связь однин-ко-многим
         //test
         public News()
         {
-            Pictures = new List<Pictures>();
+            NewsPictures = new List<NewsPictures>();
         }
 
     }
